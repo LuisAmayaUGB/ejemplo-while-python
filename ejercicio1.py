5135a3c76b12e03c93c390b4d86075f6b3bbe344
luis antonio amaya villalta <amayantonio@hotmail.com>
2021-07-29 17:07:41 -0600

    mensaje

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,8 @@
-print("Hola")
+import time
+numero = int(input("Escriba un número    0 para salir: "))
+suma=0
+while (numero != 0):
+    suma+=numero
+    numero = int(input("Escriba un número: "))
+print(suma)
+time.sleep(10)
